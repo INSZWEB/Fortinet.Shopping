19c5c537e7e2c827456920331d247fd43bbd370f
INSZWEB <marketing@insightz.tech>
2025-06-17 22:58:28 +0800

    Add files via upload

---
+++ .next/static/chunks/app_layout_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9bc2cf46._.css",
+  "static/chunks/_a4d8707d._.js"
+],
+    source: "dynamic"
+});
